ed7409b8b63df9670b5cd8e04522dc5e47d144ce
Hirotaka Kawashima <k.hirotaka@me.com>
2019-11-23 14:43:37 +0900

    update for build v0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='wallflower',
-    version='0.1',
+    version='0.2',
     install_requires=["numpy", "scipy"],
     packages=find_packages(),
     url='https://github.com/khirotaka/wallflower',
